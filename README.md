ce64108b431adf43cb3dc6c774b7bbf6cddd4470
ailtonbsj <ailton.ifce@gmail.com>
2013-08-08 11:30:00 -0300

    Sample using the button from FRDM-TFC

---
+++ README.md
@@ -0,0 +1,8 @@
+# Processor Expert Samples
+
+## Require
+
+- CodeWarrior for MCU 10.4 [Download here!](https://drive.google.com/open?id=1CAAZywCCLJJJ9dUxb6GMovbbtYWvdGv3)
+- Freedom Board (Freescale Processor Kinetis KL25Z)
+- FRDM-TFC Board
+- Linescan Sensor
---
+++ ToggleLedSample1/Sources/Events.h
@@ -0,0 +1,99 @@
+/* ###################################################################
+**     Filename    : Events.h
+**     Project     : ProcessorExpert
+**     Processor   : MKL25Z128VLK4
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2013-08-07, 21:25, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Cpu_OnNMIINT - void Cpu_OnNMIINT(void);
+**
+** ###################################################################*/
+/*!
+** @file Events.h
+** @version 01.00
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+
+#ifndef __Events_H
+#define __Events_H
+/* MODULE Events */
+
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+#include "TimerInt1.h"
+#include "TimerIntLdd1.h"
+#include "TU1.h"
+#include "Led1.h"
+#include "BitIoLdd1.h"
+#include "Button1.h"
+#include "BitIoLdd2.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+/*
+** ===================================================================
+**     Event       :  Cpu_OnNMIINT (module Events)
+**
+**     Component   :  Cpu [MKL25Z128LK4]
+**     Description :
+**         This event is called when the Non maskable interrupt had
+**         occurred. This event is automatically enabled when the <NMI
+**         interrupt> property is set to 'Enabled'.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void Cpu_OnNMIINT(void);
+
+
+/*
+** ===================================================================
+**     Event       :  TimerInt1_OnInterrupt (module Events)
+**
+**     Component   :  TimerInt1 [TimerInt]
+**     Description :
+**         When a timer interrupt occurs this event is called (only
+**         when the component is enabled - <Enable> and the events are
+**         enabled - <EnableEvent>). This event is enabled only if a
+**         <interrupt service/event> is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void TimerInt1_OnInterrupt(void);
+
+/* END Events */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+#endif 
+/* ifndef __Events_H*/
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.2 [05.07]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
